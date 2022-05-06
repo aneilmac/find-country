72702d39399afde13084cdacb167cb0688641fa8
Archibald Neil MacDonald <archibaldnmac@gmail.com>
2022-05-06 02:53:00 +0100

    Updating valid list.

--- src/countryCodes.ts
+++ src/countryCodes.ts
@@ -270,7 +270,6 @@ export const validDeletable : CountryData[] = [
     {"code": "BE", "name": "Belgium"},
     {"code": "BF", "name": "Burkina Faso"},
     {"code": "BG", "name": "Bulgaria"},
-    {"code": "BH", "name": "Bahrain"},
     {"code": "BI", "name": "Burundi"},
     {"code": "BJ", "name": "Benin"},
     {"code": "BO", "name": "Bolivia"},
